f4d0ee72121f231bd61b3975554e0350a7880482
Bogdan Valentin <bogdanvalentin977@gmail.com>
2023-01-04 14:55:46 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "ncurses.h"
+
+int main()
+{
+    init_curses();
+    init_header();
+    int rasp = 0;
+    do {
+        
+    } while(rasp == 1);
+    uninit_curses();
+    return 0;
+}
---
+++ ncurses.h
@@ -0,0 +1,11 @@
+#pragma once
+
+void init_curses();
+
+void uninit_curses();
+
+void init_header();
+
+void init_footer();
+
+void init_mainwindow();
